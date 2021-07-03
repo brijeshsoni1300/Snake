47cdb13e34a42b8b62b71042abaaff259d39205c
Brijesh Patadiya <brijeshsoni1300@gmail.com>
2021-07-03 10:24:59 +0530

    snake game

---
+++ src/GameFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class GameFrame extends JFrame {
+
+    GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+//    @Override
+//    public void actionPerformed(ActionEvent e){
+//
+//    }
+}
